fc36bf8a197c21b265480943667e3d76c3dfbed9
GrPaladino <paladino.graziano@gmail.com>
2024-01-11 17:05:46 +0100

    Fix: removed comments

--- Js/main.js
+++ Js/main.js
@@ -1,28 +1,25 @@
-// - creare un ciclo for per creare dei div da 1 a 100;
-// - creare un consol.log per vederli in console;
-// - creare la formula per cambiare il contenuto dell'elemento in base al numero;
-// - creare l'elemento html e il suo stile in cui creare i box;
-// - recuperarlo in js;
-// - stampare i box nell'elemento html;
-// - applica gli stili differenti a seconda del valore inserito.
+// - recuperare elemento in js;
 const boxContainer = document.getElementById("box-container");
 
+// - creare la formula per cambiare il contenuto dell'elemento in base al numero;
 for (let i = 1; i <= 100; i++) {
   let textBox = i;
 
   if (i % 15 == 0) {
     textBox = "FizzBuzz";
-    // boxElement.style.backgroundColor = "#f0456e";
   } else if (i % 5 == 0) {
     textBox = "Buzz";
   } else if (i % 3 == 0) {
     textBox = "Fizz";
   }
-
+  // - creare elemento html da inserire;
   boxContainer.innerHTML += `
   <div class="box ${textBox}"> 
-  <h3>
-  ${textBox}
-  </h3>
+    <h3>
+        ${textBox}
+    </h3>
   </div>`;
+
+  // - creare un consol.log per vederli in console;
+  console.log(textBox);
 }
