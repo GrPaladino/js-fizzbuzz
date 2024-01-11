ef801ef7562af31ee73c50dbd4fd20b797688c9f
GrPaladino <paladino.graziano@gmail.com>
2024-01-11 15:53:57 +0100

    Feat: create  100 box

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,28 @@
+// - creare un ciclo for per creare dei div da 1 a 100;
 // - creare un consol.log per vederli in console;
 // - creare la formula per cambiare il contenuto dell'elemento in base al numero;
 // - creare l'elemento html e il suo stile in cui creare i box;
 // - recuperarlo in js;
 // - stampare i box nell'elemento html;
 // - applica gli stili differenti a seconda del valore inserito.
+const boxContainer = document.getElementById("box-container");
 
-// - creare un ciclo for per creare dei div da 1 a 100;
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
+  //   let textBox = "";
+
+  boxContainer.innerHTML += `
+  <div class="box"> 
+    <h3>
+        ${i}
+    </h3>
+  </div>`;
+  console.log(boxContainer);
+
+  //   if (i % 15 == 0) {
+  //     textBox = "Fizzbuzz";
+  //   } else if (i % 5 == 0) {
+  //     textBox = "Buzz";
+  //   } else if (i % 3 == 0) {
+  //     textBox = "Fizz";
+  //   }
 }
