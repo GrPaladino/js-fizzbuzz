5dc216e2cf678af787fe4ee27dfe2ff679a3d426
GrPaladino <paladino.graziano@gmail.com>
2024-01-11 15:06:07 +0100

    Feat: add costrutto e creato il ciclo

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+// - creare un consol.log per vederli in console;
+// - creare la formula per cambiare il contenuto dell'elemento in base al numero;
+// - creare l'elemento html e il suo stile in cui creare i box;
+// - recuperarlo in js;
+// - stampare i box nell'elemento html;
+// - applica gli stili differenti a seconda del valore inserito.
+
+// - creare un ciclo for per creare dei div da 1 a 100;
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
